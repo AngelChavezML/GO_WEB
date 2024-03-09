c52ec553ab5e2e1df0c9997531557653b7ba744c
Angel Uriel Chavez <angel.chavezmorones@mercadolibre.com.mx>
2024-03-09 15:44:32 -0600

    Adding Get Routes

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
 
 // Define a struct
-type Products struct {
+type Products_Struct struct {
 	ID           int
 	Name         string
 	Quantity     int
@@ -21,7 +21,7 @@ type Products struct {
 	Price        float64
 }
 
-func Charge_Products() []Products {
+func Charge_Products() []Products_Struct {
 	//Read JSON file
 	file, err := os.Open("products.json")
 	if err != nil {
@@ -29,7 +29,7 @@ func Charge_Products() []Products {
 	}
 	defer file.Close()
 	//Decode JSON file
-	var products []Products
+	var products []Products_Struct
 	err = json.NewDecoder(file).Decode(&products)
 	if err != nil {
 		log.Fatal(err)
@@ -39,13 +39,67 @@ func Charge_Products() []Products {
 func main() {
 	Products := Charge_Products()
 	//Show Products
-	fmt.Println(Products)
+	//fmt.Println(Products)
 	//Begin server
 	router := chi.NewRouter()
 	//Get that shows pong and 200 OK
 	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("pong"))
+	})
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+	//Get that shows all products
+	router.Get("/products", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Products)
+	})
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+	//Get that shows a product by ID
+	router.Get("/products/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		id := chi.URLParam(r, "id")
+		productID, err := strconv.Atoi(id)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Invalid product ID"))
+			return
+		}
+		for _, product := range Products {
+			if product.ID == productID {
+				json.NewEncoder(w).Encode(product)
+				return
+			}
+		}
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 - Product not found"))
+	})
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+	//Get that seach products that their price is higher than a given price
+	router.Get("/products/search/{priceGt}", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		priceGt := chi.URLParam(r, "priceGt")
+		priceGtFloat, err := strconv.ParseFloat(priceGt, 64)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Invalid price"))
+			return
+		}
+		var productsFound []Products_Struct
+		for _, product := range Products {
+			if product.Price > priceGtFloat {
+				productsFound = append(productsFound, product)
+			}
+		}
+		json.NewEncoder(w).Encode(productsFound)
 	})
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		panic(err)
