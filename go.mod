099ab0efca4a47d00b95ac23d3f87f9f868b69e8
Angel Uriel Chavez <angel.chavezmorones@mercadolibre.com.mx>
2024-03-09 15:33:45 -0600

    Adding CHI and Get Route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module proyect/go-web
 
 go 1.22.0
+
+require github.com/go-chi/chi/v5 v5.0.12 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
 )
 
 // Define a struct
@@ -37,4 +40,15 @@ func main() {
 	Products := Charge_Products()
 	//Show Products
 	fmt.Println(Products)
+	//Begin server
+	router := chi.NewRouter()
+	//Get that shows pong and 200 OK
+	router.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		panic(err)
+	}
+
 }
